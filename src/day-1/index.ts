ac5107d3b1cc0768a76e9957d6eeb89724010eeb
Mika Hiltunen <mika.hiltunen@smartly.io>
2020-11-30 07:49:35 +0200

    Helpers and example from 2019

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -1,5 +1,15 @@
+import { readInputAsNumbers } from '../helpers';
+
+const inputFile = `${__dirname}/input.txt`;
+
+const calculateModuleFuel = (mass: number): number => {
+    return Math.floor(mass/3)-2
+}
+
 const run = () => {
-    console.log("Day 1!")
+    const input = readInputAsNumbers(inputFile);
+    const fuel = input.reduce((acc, value) => acc + calculateModuleFuel(value), 0)
+    console.log(fuel)
 }
 
 export default run;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import run from './day-1'
+import day1 from './day-1'
 
-run();
+day1();

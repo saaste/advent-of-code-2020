027d87ebb5a235f54dfaf6c146db0f3a9c064882
Mika Hiltunen <mika.hiltunen@smartly.io>
2020-12-03 07:29:13 +0200

    Day 3

---
+++ src/day-3/index.ts
@@ -0,0 +1,44 @@
+import { readInput } from '../helpers';
+
+const inputFile = `${__dirname}/input.txt`;
+const map = readInput(inputFile);
+
+const countTrees = (right: number, down: number): number => {
+    let trees = 0;
+    let positionX = 0;
+
+    for (let i = 0; i < map.length; i+=down) {
+
+        while (map[i].length < positionX + right) {
+            map[i] = map[i] + map[i];
+        }
+
+        if (map[i][positionX] === '#') {
+            trees++;
+        }
+        
+        positionX += right;
+    }
+
+    return trees;
+}
+
+export const day3_step_1 = (right: number = 3, down: number = 1, log: boolean = true) => {
+    const trees = countTrees(3, 1);
+
+    console.log('Step 1');
+    console.log('------');
+    console.log(`Trees: ${trees}`);
+}
+
+export const day3_step_2 = () => {
+    const trees = [[1,1], [3,1], [5,1], [7,1], [1,2]].map((values) => {
+        return countTrees(values[0], values[1]);
+    })
+
+    const result = trees.reduce((a, b) => a * b);
+
+    console.log('Step 2');
+    console.log('------');
+    console.log(`Trees: ${trees}. Multiplied: ${result}`)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import {day1_step_1, day1_step_2} from './day-1'
 import {day2_step_1, day2_step_2} from './day-2'
+import {day3_step_1, day3_step_2} from './day-3'
 
 // day1_step_1();
 // console.log()
 // day1_step_2();
 
-day2_step_1();
+// day2_step_1();
+// console.log()
+// day2_step_2();
+
+day3_step_1();
 console.log()
-day2_step_2();
+day3_step_2();
